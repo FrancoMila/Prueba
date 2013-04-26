2ddb3d1dd42a2618074d562613e50844c7f9077b
FrancoMila <franco_riverplate91@hotmail.com>
2013-04-26 18:41:46 -0300

    Modificado 2

--- Pruebita.c
+++ Pruebita.c
@@ -7,7 +7,10 @@
 #include <stdio.h>
 
 int main (void) {
+	char buffer[80];
 	printf("hola que tal");
+	gets (buffer);
+	printf("Contenido: %s", buffer);
 	printf("Mira como te lo cambio gil");
 	return 0;
 }
